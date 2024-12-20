e6bf306313b1ded69bc039faf8d3dc248feb2008
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-21 00:55:19 +0600

    1

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
-
 import express from 'express';
-
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route.js';
+import { connect } from 'mongoose';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -16,4 +16,5 @@ app.use("/api/auth",authRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on PORT:', PORT);
+    connectDB();
     });
